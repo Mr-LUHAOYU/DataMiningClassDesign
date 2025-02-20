a13fb0b553b2d1eaa8d29037a58637f2f882816e
2656670854 <2656670854@qq.com>
2025-02-20 14:07:28 +0800

    pre_pro

--- src/pre_process.py
+++ src/pre_process.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-random_state = 42
 train_size, val_size, test_size = 0.6, 0.2, 0.2
 
 df = pd.read_csv('../dataset/encoded_data.csv')
@@ -12,8 +11,8 @@ rare = normalized[normalized['HadHeartAttack'] == 1]
 
 
 def split_data(data, train_size=0.6, val_size=0.2, test_size=0.2):
-    train_val, test = train_test_split(data, test_size=test_size, random_state=random_state)
-    train, val = train_test_split(train_val, test_size=val_size / (train_size + val_size), random_state=random_state)
+    train_val, test = train_test_split(data, test_size=test_size)
+    train, val = train_test_split(train_val, test_size=val_size / (train_size + val_size))
     return train, val, test
 
 
@@ -24,10 +23,14 @@ train = pd.concat([train_common, train_rare])
 val = pd.concat([val_common, val_rare])
 test = pd.concat([test_common, test_rare])
 
-train = train.sample(frac=1, random_state=random_state).reset_index(drop=True)
-val = val.sample(frac=1, random_state=random_state).reset_index(drop=True)
-test = test.sample(frac=1, random_state=random_state).reset_index(drop=True)
+train = train.sample(frac=1).reset_index(drop=True)
+val = val.sample(frac=1).reset_index(drop=True)
+test = test.sample(frac=1).reset_index(drop=True)
 
-train.to_csv('../dataset/train.csv', index=False)
-val.to_csv('../dataset/val.csv', index=False)
-test.to_csv('../dataset/test.csv', index=False)
+target_dir = '../dataset/data0'
+import os
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
+train.to_csv(f'{target_dir}/train.csv', index=False)
+val.to_csv(f'{target_dir}/val.csv', index=False)
+test.to_csv(f'{target_dir}/test.csv', index=False)
